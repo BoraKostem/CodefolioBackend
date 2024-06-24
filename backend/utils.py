b1a321a2d8e66423989915ab513405739bb9968d
BoraKostem <bora_kostem@outlook.com>
2024-06-24 14:45:46 +0300

    refac: added environment get values for mongodb client

--- backend/utils.py
+++ backend/utils.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
-
+import os
 def get_db_handle(db_name, host, port, username, password):
 
- client = MongoClient(host=host,
-                      port=int(port),
-                      username=username,
-                      password=password
+ client = MongoClient(host= os.getenv('MONGO_HOST'),
+                      port=int(os.getenv('MONGO_PORT')),
+                      username= os.getenv('MONGO_USER'),
+                      password= os.getenv('MONGO_PASS'),
                      )
- db_handle = client['db_name']
+ db_handle = client['codefolio']
  return db_handle, client
