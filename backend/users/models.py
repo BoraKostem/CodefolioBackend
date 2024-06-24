68e2c5c6969c8165cb04a414e03f83689737e2db
BoraKostem <bora_kostem@outlook.com>
2024-06-24 15:25:34 +0300

    update: updated model file for the current database schema

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,5 @@
-# Create your models here.
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class MyUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -19,6 +18,13 @@ class MyUserManager(BaseUserManager):
 
 class MyUser(AbstractBaseUser):
     email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    linkedin_url = models.URLField(blank=True, null=True)
+
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -26,3 +32,33 @@ class MyUser(AbstractBaseUser):
     objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+class CVLanguage(models.Model):
+    user = models.ForeignKey(MyUser, related_name='cv_languages', on_delete=models.CASCADE)
+    language = models.CharField(max_length=255)
+    proficiency = models.CharField(max_length=255)
+
+class CVInformation(models.Model):
+    user = models.ForeignKey(MyUser, related_name='cv_information', on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    info = models.TextField()
+
+class CVProjectLanguage(models.Model):
+    language = models.CharField(max_length=255)
+    project = models.ForeignKey('CVProject', related_name='cv_projects_languages', on_delete=models.CASCADE)
+
+class CVProject(models.Model):
+    user = models.ForeignKey(MyUser, related_name='cv_projects', on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class GitHubProjectLanguage(models.Model):
+    language = models.CharField(max_length=255)
+    percentage = models.FloatField()
+    project = models.ForeignKey('GitHubProject', related_name='github_projects_languages', on_delete=models.CASCADE)
+
+class GitHubProject(models.Model):
+    user = models.ForeignKey(MyUser, related_name='github_projects', on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=255)
+    description = models.TextField()
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,15 +1,59 @@
 from rest_framework import serializers
-from .models import MyUser
+from .models import MyUser, CVLanguage, CVInformation, CVProject, CVProjectLanguage, GitHubProject, GitHubProjectLanguage
+
+class CVLanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CVLanguage
+        fields = ['id', 'language', 'proficiency']
+
+class CVInformationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CVInformation
+        fields = ['id', 'headline', 'info']
+
+class CVProjectLanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CVProjectLanguage
+        fields = ['id', 'language']
+
+class CVProjectSerializer(serializers.ModelSerializer):
+    cv_projects_languages = CVProjectLanguageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CVProject
+        fields = ['id', 'project_name', 'description', 'cv_projects_languages']
+
+class GitHubProjectLanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GitHubProjectLanguage
+        fields = ['id', 'language', 'percentage']
+
+class GitHubProjectSerializer(serializers.ModelSerializer):
+    github_projects_languages = GitHubProjectLanguageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = GitHubProject
+        fields = ['id', 'project_name', 'description', 'github_projects_languages']
 
 class UserSerializer(serializers.ModelSerializer):
+    cv_languages = CVLanguageSerializer(many=True, read_only=True)
+    cv_information = CVInformationSerializer(many=True, read_only=True)
+    cv_projects = CVProjectSerializer(many=True, read_only=True)
+    github_projects = GitHubProjectSerializer(many=True, read_only=True)
+
     class Meta:
         model = MyUser
-        fields = ['id', 'email', 'password']
+        fields = ['id', 'email', 'name', 'location', 'phone', 'github_url', 'linkedin_url', 'cv_languages', 'cv_information', 'cv_projects', 'github_projects', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = MyUser.objects.create_user(
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            name=validated_data['name'],
+            location=validated_data.get('location', ''),
+            phone=validated_data.get('phone', ''),
+            github_url=validated_data.get('github_url', ''),
+            linkedin_url=validated_data.get('linkedin_url', '')
         )
         return user
